f8ee12d119208e4dbdc5b546daebba804502551a
J. Michael Dean <mdean77@comcast.net>
2022-06-25 07:32:51 -0600

    Initial Commit

---
+++ Shared/MacPuf2022PApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacPuf2022PApp.swift
+//  Shared
+//
+//  Created by J Michael Dean on 6/25/22.
+//
+
+import SwiftUI
+
+@main
+struct MacPuf2022PApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
