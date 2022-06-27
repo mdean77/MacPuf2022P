0202ede63dc85a08710daeec242d0cf397613029
J. Michael Dean <mdean77@comcast.net>
2022-06-27 13:28:43 -0600

    Moved some debug console code into extension, added dump routines.

--- Shared/Human.swift
+++ Shared/Human.swift
@@ -4,162 +4,54 @@
 //
 //  Created by J Michael Dean on 6/25/22.
 //
+//  This is my MODEL.  It will be created by my VIEWMODEL which is called Simulator.
 
 import Foundation
 
 struct Human {
-    //MARK: Initialization
-    // New organization - returning to original FORTRAN variables and initialization
-    var FIO2 = 20.93000                         // MacPuf variable FIO2   Factor 1
-    var FIC2 = 0.0300                           // MacPuf variable FIC2   Factor 2
-    var CO = 100.000                            // MacPuf variable CO        Factor 3
-    var PD = 100.0                              // PD  Factor 4
-    var FADM = 0.0                              // MacPuf variable FADM        Factor 5
-    var BULLA = 0.0000                          // MacPuf variable BULLA  Factor 6
-    var VLUNG = 3000.000                        // MacPuf variable VLUNG  Factor 7
-    var ELAST = 5.0000                          // MacPuf variable ELAST  Factor 8
-    var VADM = 3.0                              // MacPuf variable VADM        Factor 9
-    var AZ = 100.000                            // MacPuf variable      Factor 10
-    var BZ = 100.000                            // MacPuf variable BZ      Factor 11
-    var CZ = 100.000                            // MacPuf variable CZ     Factor 12
-    var BARPR = 760.0                           // BARPR  Factor 13
-    var TEMP = 37.0                             // TEMP Factor 14
-    var TRQ = 0.8000                            // TRQ   Factor 15
-    var TC2MT = 13.38                           // TC2MT Factor 16
-    var TVOL = 12.0000                          // TVOL Factor 17
-    var HB = 14.8                               // HB  Factor 18
-    var PCV = 45.0                              // PCV Factor 19
-    var VBLVL = 3000.00                         // MacPuf variable VBLVL  Factor 20
-    var ADDC3 = 0.0                             // MacPuf variable ADDC3  Factor 21
-    var BC3AJ = 0.0                             //BC3AJ Factor 22
-    var DPG = 3.8                               // DPG Factor 23
-    var PR = 100.0                              //  PR  Factor 24  - same as ventilatory coupling
-    var FITNS = 33.0                            // FITNS  Factor 25
-    var SPACE = 0.4000                          // % of respiratory cycle spent in inspiration SPACE Factor 26
-    var COMAX = 35.0000                         // MacPuf variable COMAX    Factor 27
-    var SHUNT = 0.0                             // MacPuf variable SHUNT    Factor 28
-    var VC = 5.0000                             // MacPuf variable VC     Factor 29
-    var PEEP = 0.0                              // PEEP  Factor 30
-    var VO2CT = 14.56                           // VO2CT  Factor 31
-    var RCOAJ = 100.0                           // Factor 32
-    var RPH = 7.4                               // MacPuf variable RPH    Factor 33
-    var VPH = 7.37                              // VPH Factor 34
-    var FVENT = 4.26                            // MacPuf variable FVENT  Factor 35
-    var BPH = 7.33                              // BPH  Factor 36
-    var BAGO = 0.0                              // MacPuf variable BAGO  Factor 37
-    var BAGC = 0.0                              // MacPuf variable BAGC  Factor 38
-    var AO2MT = 348.1                           // MacPuf variable AO2MT  Factor 39
-    var AC2MT = 143.3                           // MacPuf variable AC2MT  Factor 40
-    var AO2PR = 101.9                           // MacPuf variable AO2PR  Factor 41
-    var AC2PR = 39.75                           // MacPuf variable AC2PR  Factor 42
-    var DPH = 7.40                              // DPH Factor 43
-    var XLACT = 0.0                             // XLACT Factor 44
-    var BO2PR = 28.89                           // BO2PR  Factor 45
-    var BC2PR = 52.8                            // BC2PR  Factor 46
-    var TIDVL = 461.9                           // MacPuf variable TIDVL  Factor 47
-    var RRATE = 12.82                           // MacPuf variable RRATE  Factor 48
-    var RO2CT = 19.54                           // MacPuf variable RO2CT  Factor 49
-    var VC2MT = 1540.0                          // VC2MT  Factor 50
-    var DVENT = 5.92                            // MacPuf variable DVENT  Factor 51
-    var SVENT = 5.92                            // MacPuf variable SVENT  Factor 52
-    var PC2CT = 47.25                           // MacPuf variable PC2CT  Factor 53
-    var PO2CT = 19.66                           // MacPuf variable PO2CT  Factor 54
-    var TO2CT = 14.47                           // TO2CT Factor 55
-    var TC2CT = 51.4                            // TC2CT Factor 56
-    var BO2CT = 10.05                           // BO2CT  Factor 57
-    var BC2CT = 56.62                           // BC2CT  Factor 58
-    var TPH = 7.37                              // TPH Factor 59
-    var RC3CT = 23.82                           // MacPuf variable RC3CT  Factor 60
-    var VC2CT = 51.33                           // VC2CT  Factor 61
-    var RO2MT = 195.44                          // MacPuf variable RO2MT  Factor 62
-    var RC2MT = 473.5                           // MacPuf variable RC2MT  Factor 63
-    var XRESP = 10.99                           // XRESP Factor 64
-    var AN2MT = 1987.0                          // MacPuf variable AN2MT  Factor 65
-    var BO2MT = 18.06                           // BO2MT  Factor 66
-    var BC2MT = 677.0                           // BC2MT  Factor 67
-    var CBF = 51.94                             // CBF   Factor 68
-    var PC  = 0.80                              // PC Factor 69 0.7993?
-    var DSPAC = 129.2                           // MacPuf variable DSPAC  Factor 70
-    var REFLV = 3000.000                        // MacPuf variable REFLV  Factor 71
-    var RO2PR = 94.18                           // MacPuf variable RO2PR  Factor 72
-    var CONSO = 240.000                         // CONSO Factor 73
-    var RC2PR = 39.9                            // MacPuf variable RC2PR  Factor 74
-    var PG = 0.0                                // PG Factor 75 0.00?
-    var PJ = 97.13                              // PJ Factor 76 97.0730 ??
-    var TND = -10.0                             // TND Factor 77 0.0851 ??  Minutes that brain has been deprived of O2 MARK: 0.0851  was in new version
-    var RC2CT = 47.35                           // MacPuf variable RC2CT  Factor 78
-    var QB = 33.29                              // QB Factor 79   Net CO2 output per unit time interval MARK: 199.5492 in old version
-    var PW = 2.3618                             // MacPuf variable PW        Factor 80
-    var FT = 0.016667                           // FT Factor 81 = 1/60
-    var CONOM = 4.6000                          // MacPuf variable CONOM    Factor 82
-    var BUBBL = 0.000                           // BUBBL Factor 83
-    var TC2RF = 45.43                           // TC2RF Factor 84
-    var TC3MT = 318.0                           // TC3MT Factor 85
-    var VC3MT = 71.55                           // VC3MT  Factor 86
-    var TC3CT = 25.48                           // TC3CT Factor 87
-    var VC3CT = 25.48                           // VC3CT  Factor 88
-    var TLAMT = 34.63                           // TLAMT  Factor 89
-    var RLACT = 0.99                            // MacPuf variable RLACT  Factor 90
-    var BC3CT = 22.7000                         // BC3CT   Factor 91
-    var BO2AD = 1.0                             // BO2AD  Factor 92  var oxygenationIndex = 1.000 ???
-    var COADJ = 5.01                            // MacPuf variable COADJ    Factor 93
-    var EO2CT = 19.54                           // MacPuf variable EO2CT  Factor 94
-    var TO2MT = 178.4                           // TO2MT Factor 95
-    var TO2PR = 40.13                           // TO2PR Factor 96
-    var TC2PR = 45.43                           // TC2PR Factor 97
-    var VO2MT = 437.0                           // VO2MT  Factor 98
-    var AVENT = 710.8                           // AVENT Factor 99  MARK: old value 366.3107
-    var PL = 0                                // PL Factor 100
-    var EC2CT = 47.35                           // MacPuf variable EC2CT  Factor 101
-    var TN2MT = 76.07                           // TN2MT - Amount in fast Factor 102
-    var TN2PR = 571.1                           // TN2PR - Fast equilibrating store Factor 103
-    var FEV = 4.0                               // MacPuf variable FEV      Factor 104
-    var SN2PR = 564.0                           // SN2PR - Slow equilibraint store Factor 105
-    var RN2CT = 0.73                            // MacPuf same as EN2CT Factor  Factor 106
-    var UN2MT = 0.0                             // UN2MT Factor 107
-    var RN2MT = 7.26                            // MacPuf variable RN2MT  Factor 108
-    var HRATE = 70.0                            // HRATE Factor 109  Heart rate
-    var STRVL = 70.0                            // STRVL Factor 110  Stroke Volume
-    var TC3AJ = -0.03                           // TC3AJ Factor 111
-    var SN2MT = 967.0                           // SN2MT - Amount in slow Factor 112
-    var QA = 41.61                              // QA Factor 113 O2 uptake per unit time interval
-    var RVADM = 0.000                           // RVADM used only when using PFT (see end of Clin2) Factor 114
-    var XDSPA = 0.000                           // MacPuf variable XDSPA  Factor Factor 115
-    var BAG = 0.0                               // MacPuf variable BAG   Factor 116
-    var XMALE = 1.0                             // XMALE Factor 117
-    var HT = 178.0                              // HT  Factor 118
-    var WT = 70.0                               // WT  Factor 119
-    var AGE = 40.0                              // AGE Factor 120
-    
-    var NARTI = 1                               // (1 = natural, 0 = artificial)
+
+    static let zeroes = (0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0,0.0,0.0)
+    // Initialize parameter variables with zero values
+    var (FIO2,FIC2,CO,PD,FADM,BULLA,VLUNG,ELAST,VADM,AZ) = zeroes
+    var (BZ,CZ,BARPR,TEMP,TRQ,TC2MT,TVOL,HB,PCV,VBLVL) = zeroes
+    var (ADDC3,BC3AJ,DPG,PR,FITNS,SPACE,COMAX,SHUNT,VC,PEEP) = zeroes
+    var (VO2CT,RCOAJ,RPH,VPH,FVENT,BPH,BAGO,BAGC,AO2MT,AC2MT) = zeroes
+    var (AO2PR,AC2PR,DPH,XLACT,BO2PR,BC2PR,TIDVL,RRATE,RO2CT,VC2MT) = zeroes
+    var (DVENT,SVENT,PC2CT,PO2CT,TO2CT,TC2CT,BO2CT,BC2CT,TPH,RC3CT) = zeroes
+    var (VC2CT,RO2MT,RC2MT,XRESP,AN2MT,BO2MT,BC2MT,CBF,PC,DSPAC) = zeroes
+    var (REFLV,RO2PR,CONSO,RC2PR,PG,PJ,TND,RC2CT,QB,PW) = zeroes
+    var (FT,CONOM,BUBBL,TC2RF,TC3MT,VC3MT,TC3CT,VC3CT,TLAMT,RLACT) = zeroes
+    var (BC3CT,BO2AD,COADJ,EO2CT,TO2MT,TO2PR,TC2PR,VO2MT,AVENT,PL) = zeroes
+    var (EC2CT,TN2MT,TN2PR,FEV,SN2PR,RN2CT,UN2MT,RN2MT,HRATE,STRVL) = zeroes
+    var (TC3AJ,SN2MT,QA,RVADM,XDSPA,BAG,XMALE,HT,WT,AGE) = zeroes
     
     // Initialize constants with zero values
-    var (c1, c2, c3, c4, c5, c6, c7, c8, c9, c10) = (0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0,0.0,0.0)
-    var (c11, c12, c13, c14, c15, c16, c17, c18, c19, c20) = (0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0,0.0,0.0)
-    var (c21, c22, c23, c24, c25, c26, c27, c28, c29, c30) = (0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0,0.0,0.0)
-    var (c31, c32, c33, c34, c35, c36, c37, c38, c39, c40) = (0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0,0.0,0.0)
-    var (c41, c42, c43, c44, c45, c46, c47, c48, c49, c50) = (0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0,0.0,0.0)
-    var (c51, c52, c53, c54, c55, c56, c57, c58, c59, c60) = (0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0,0.0,0.0)
-    var (c61, c62, c63, c64, c65, c66, c67, c68, c69, c70) = (0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0,0.0,0.0)
+    var (c1, c2, c3, c4, c5, c6, c7, c8, c9, c10) = zeroes
+    var (c11, c12, c13, c14, c15, c16, c17, c18, c19, c20) = zeroes
+    var (c21, c22, c23, c24, c25, c26, c27, c28, c29, c30) = zeroes
+    var (c31, c32, c33, c34, c35, c36, c37, c38, c39, c40) = zeroes
+    var (c41, c42, c43, c44, c45, c46, c47, c48, c49, c50) = zeroes
+    var (c51, c52, c53, c54, c55, c56, c57, c58, c59, c60) = zeroes
+    var (c61, c62, c63, c64, c65, c66, c67, c68, c69, c70) = zeroes
+    var (c71, c72, c73, c74, c75) = (0.0, 0.0, 0.0, 0.0, 0.0)
     
-    // Replace constants with the correct constant values
+    var NARTI = 1
+    var SIMLT = 1.0
+    
+    // Replace parameters with correct values from extension
+    mutating func getVariables(){
+        setVariables()
+    }
+
+    // Replace constants with the correct constant values from extension
     mutating func getConstants (){
         setConstants()
     }
-    
-    
-    
-    
     
     mutating func simulate(cycle:Int, iterations:Int){
 
         
         if cycle == iterations {
-            print(c1)
-            print(c2)
-            print(c3)
-            print(c4)
-            print(c70)
             ADDC3 = 0
         }
     }
--- Shared/Simulator.swift
+++ Shared/Simulator.swift
@@ -4,97 +4,20 @@
 //
 //  Created by J Michael Dean on 6/25/22.
 //
+//  This is my ViewModel class which mediates between the model (Human) and the views.
 
 import Foundation
 
-struct Simulator {
+class  Simulator {
     var iterations = 1800
     var intervalFactor = 300
     var totalSeconds = -1
     var human = Human()
-
     
-    func cycleReport() -> String{
-        // Returns a string for single line of output
-        let scale = 120.0
-        let lineWidth = 72.0
-        let factor = lineWidth/scale
-        let po2 = human.RO2PR < scale ? Int(human.RO2PR*factor) : Int(lineWidth)
-        let pco2 = human.RC2PR < scale ? Int(human.RC2PR*factor) : Int(lineWidth)
-        let vent = Int(human.DVENT*factor)  // Replace with lungs.totalVentilation when I have lungs
-        let rate = Int(human.RRATE*factor)  // Replace with lungs.respiratoryRate when I have lungs
-        
-        let seconds = totalSeconds % 60
-        let minutes = totalSeconds / 60
-        var temp = String(repeating: " ", count: 73)
-        
-        let oxygenIndex = temp.index(temp.startIndex, offsetBy:po2)
-        let carbonDioxideIndex = temp.index(temp.startIndex, offsetBy:pco2)
-        let rateIndex = temp.index(temp.startIndex, offsetBy: rate)
-        let ventIndex = temp.index(temp.startIndex, offsetBy: vent)
-        
-        temp.replaceSubrange(oxygenIndex...oxygenIndex, with:"O")
-        temp.replaceSubrange(carbonDioxideIndex...carbonDioxideIndex, with:"C")
-        temp.replaceSubrange(rateIndex...rateIndex, with:"F")
-        temp.replaceSubrange(ventIndex...ventIndex, with:"V")
-        let result = String(format:"%4d:%2d     ",minutes, seconds).appending(temp)
-        return(result)
-    }
-    
-    func runReport()->String{
-        // This prints description after the iterations have been run
-        let x = 10*pow(2,(8.0 - human.RPH)*3.33)
-        let result = String(format:"\nFinal values for this run were...\n\n")
-        let result1 = String(format:"Arterial pO2 = %6.1f              O2 Cont =%6.1f      O2 Sat = %5.1f%%\n",human.RO2PR, human.RO2CT,human.PJ)
-        let result2 = String(format:"Arterial pCO2 = %5.1f              CO2 Cont =%6.1f\n",human.RC2PR,human.RC2CT)
-        let result3 = String(format:"Arterial pH = %7.2f (%3.1f nm)    Arterial bicarbonate = %5.1f\n\n",human.RPH,x,human.RC3CT)
-        let result4 = String(format:"Respiratory rate =  %5.1f          Tidal vol.= %6.0f ml\n", human.RRATE, human.TIDVL)
-        let result5 = String(format:"Total ventilation = %5.1f l/min    Actual cardiac output = %4.1f l/min\n",human.DVENT, human.COADJ)
-        let result6 = String(format:"Total dead space =   %4.0f ml       Actual venous admixture = %3.1f%%\n",human.DSPAC,human.PW)
-        let result7 = String(format:"Cardiac output =    %5.1f l/min    Cerebral blood flow = %5.1f ml/100g/min\n", human.COADJ, human.CBF)
-        let result8 = String(format:"Heart rate =        %5.1f          Stroke volume = %5.1f ml\n", human.HRATE, human.STRVL)
-        return result + result1 + result2 + result3 + result4 + result5 + result6 + result7 + result8
-    }
-    
-    
-    func dumpParametersReport() -> String {
-        // Print out first 6 changeable parameters
-        
-        let result = String(format:"\nList of first six parameters:\n     Inspired O2 %%:%6.1f\n     Inspired CO2 %%:%6.2f\n     Cardiac performance %% of normal:%7.2f\n     Metabolic rate %% of normal:%7.2f\n     Extra anatomic right to left shunt:%6.2f\n     Extra dead space:%6.2f",human.FIO2,human.FIC2,human.CO,human.PD,human.FADM, human.BULLA)
-        return result;
-    }
-    
-    func inspectionReport()->String{
-        let header = String(format:"\nTime         P.Pressures     Contents cc/dl  Amounts in cc  pH    HCO3-\n%4d secs     O2     CO2      O2     CO2       O2     CO2", totalSeconds+1)
-        
-        let artString = String(format:"\nArterial %8.1f%8.1f%8.1f%8.1f%8.0f%8.0f%7.3f%6.1f",
-                               human.RO2PR, human.RC2PR,human.RO2CT,human.RC2CT,human.RO2MT,human.RC2MT,human.RPH,human.RC3CT)
-        let result1 = String(format:"\nAlv./Lung%8.1f%8.1f    (Sat = %4.1f%%) %6.0f%8.0f",human.AO2PR, human.AC2PR,human.PJ,human.AO2MT,human.AC2MT)
-        let result2 = String(format:"\n(Pulm.cap)%7.1f%8.1f%8.1f%8.1f",human.AO2PR, human.AC2PR,human.PO2CT,human.PC2CT)
-        let lungString = result1 + result2
-        let brainString = String(format:"\nBrain/CSF%8.1f%8.1f%8.1f%8.1f%8.0f%8.0f%7.3f%6.1f",human.BO2PR, human.BC2PR,human.BO2CT, human.BC2CT,human.BO2MT,human.BC2MT,human.BPH,human.BC3CT + human.BC3AJ)
-
-        let tissueString = String(format:"\nTissue/ECF%7.1f%8.1f%8.1f%8.1f%8.0f%8.0f%7.3f%6.1f",human.TO2PR, human.TC2PR,human.TO2CT, human.TC2CT,human.TO2MT, human.TC2MT*1000, human.TPH, human.TC3CT)
-        let veinString = String(format:"\nMixed Ven. (same as tissue)%6.1f%8.1f%8.0f%8.0f%7.3f%6.1f",human.VO2CT, human.VC2CT, human.VO2MT, human.VC2MT, human.TPH, human.TC3CT)
-        let lactateString = String(format:"\nPlasma lactate conc.= %4.2f mmol/l", human.RLACT)
-        let inspiratoryTime = 60 * human.SPACE/human.RRATE
-        let ieRatio = human.SPACE/(1 - human.SPACE)
-        let ventilatorString = human.NARTI == 1 ? "" :
-        String(format:"\nVentilator on. PEEP = %2.0f    Inspiratory time = %4.1f secs   I:E = %4.2f",human.PEEP, inspiratoryTime, ieRatio)
-        let bagString = human.PL == 0 ? "" : String(format:"\nDouglas bag connected. Volume = %4.0f ml, O2 = %4.0f ml, CO2 = %4.0f ml", human.BAG, human.BAGO, human.BAGC)
-        let result = header + artString + lungString + brainString + tissueString + veinString + lactateString +  ventilatorString + bagString
-        return(result)
-    }
-    
-    func reportOut(title: String){
-        print(title)
-        print(runReport())
-        print(inspectionReport())
-        print(dumpParametersReport())
-    }
-    
-    mutating func simulate(){
+    func simulate(){
+        human.getVariables()    // Initialization once - these are recalculated during simulation
         human.getConstants()    // only happens once per run instead of every iteration
+
         iterations = iterations >= intervalFactor ? iterations : intervalFactor
         reportOut(title:"Initial conditions before starting simulations:")
         print("\n   Time     0     10    20    30    40    50    60    70    80    90   100   110   120")
